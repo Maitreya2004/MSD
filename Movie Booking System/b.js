57443efe2a1cfa2950c1e27732cb03ecf1b9b26c
Maitreya <pasupuletimaitreya@gmail.com>
2025-04-09 20:53:58 +0530

    Create b.js

---
+++ Movie Booking System/b.js
@@ -0,0 +1,10 @@
+// initialize movie details
+const movieName = "The Dark Knight";
+const starring = ["Christian Bale", "Heath Ledger", "Aaron Eckhart"];
+const language = "English";
+const ratings = 9.0;
+// display movie details using template literals
+console.log(`Movie Name: ${movieName}`);
+console.log(`Starring: ${starring.join(", ")}`);
+console.log(`Language: ${language}`);
+console.log(`Ratings: ${ratings}`);
